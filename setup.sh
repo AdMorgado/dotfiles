480875340dab15731588568616300c4812a9644a
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2021-11-14 13:17:46 +0000

    partially added ros installation

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 
-# Elevate to sudo first
-echo "Elevating to sudo"
-
-
 
 #install packages
 sudo apt install vim
 sudo apt install g++ 
 sudo apt install git
 sudo apt install curl
+
+#install ROS
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install-ros-noetic-desktop-full
 
 
 # zsh releated
@@ -21,11 +23,16 @@ sudo apt-get install zsh
 ## Install powerlevel10k theme
 #git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# ROS Setup
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
 
 
 # catkin Setup
-
+mkdir ~/catkin_ws
+mkdir ~/catkin_ws/src
 
 
 
