d68309de6b5e4f1d23ed5c61131bac8de89948c6
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-04-11 21:38:28 +0100

    add g++-11 to setup

--- setup.sh
+++ setup.sh
@@ -17,6 +17,10 @@ sudo apt install nodejs npm
 # Latex
 sudo apt install texlive-full texlive-latex-base texlive-extra-utils texlive-science texlive-bibtex-extra texlive-fonts-extra texlive-lang-portuguese
 
+# g++-11
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt install -y g++-11
+
 # ROS Noetic
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
